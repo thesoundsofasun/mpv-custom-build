a90c47a17e7a580cd112b5a8fed575cdfe07469a
thesoundsofasun <thesoundsofasun@gmail.com>
2026-08-07 21:40:56 +0400

    Update idle_ui.lua

--- config/scripts/idle_ui.lua
+++ config/scripts/idle_ui.lua
@@ -5,7 +5,7 @@ local function update_idle()
     if mp.get_property_native("idle-active") then
         -- \an5 centers the text. 
         -- \c&HFF8800& makes the text a beautiful blue (ASS color format is Blue-Green-Red)
-        local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HFF8800&}MPV PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
+        local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HB9802A&}MPV PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
         overlay.data = ass
         overlay:update()
     else
