480928ba541cd4ed9d16a95534fa1654e7a0e88f
Sun Ra <thesoundsofasun@gmail.com>
2026-08-27 03:19:14 +0400

    Update idle_ui.lua

--- config/scripts/idle_ui.lua
+++ config/scripts/idle_ui.lua
@@ -3,15 +3,19 @@ local overlay = mp.create_osd_overlay("ass-events")
 
 local function update_idle()
     if mp.get_property_native("idle-active") then
-        -- \an5 centers the text. 
-        -- \c&HFF8800& makes the text a beautiful blue (ASS color format is Blue-Green-Red)
+        
+        -- ENGINE LEVEL KILL SWITCH: Physically disables the default UI component
+        mp.set_property("osc", "no")
+        
+        -- Draw your custom text
         local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HB9802A&}MPV PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
         overlay.data = ass
         overlay:update()
     else
+        -- Physically turn the default UI back on when music starts
+        mp.set_property("osc", "yes")
         overlay:remove()
     end
 end
 
--- Listens for when MPV stops playing media, and instantly draws the text
 mp.observe_property("idle-active", "bool", update_idle)
