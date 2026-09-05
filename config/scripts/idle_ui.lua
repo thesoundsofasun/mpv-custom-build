d26a2d6727ee9b05b4e5676a556b99cdd59335ae
Sun Ra <thesoundsofasun@gmail.com>
2026-09-05 17:02:46 +0400

    Update idle_ui.lua

--- config/scripts/idle_ui.lua
+++ config/scripts/idle_ui.lua
@@ -4,7 +4,7 @@ local overlay = mp.create_osd_overlay("ass-events")
 local function update_idle()
 if mp.get_property_native("idle-active") then
     -- Draw your custom blue text
-    local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HFF8800&}MPV AUDIO PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
+    local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HFF8800&}MPV MEDIA PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
     overlay.data = ass
     overlay:update()
     else
