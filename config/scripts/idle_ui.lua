77c67ed264ec8c59f58ba1b139f48b15ae4c0752
Sun Ra <thesoundsofasun@gmail.com>
2026-09-05 16:46:24 +0400

    Update idle_ui.lua

--- config/scripts/idle_ui.lua
+++ config/scripts/idle_ui.lua
@@ -2,20 +2,15 @@ local mp = require 'mp'
 local overlay = mp.create_osd_overlay("ass-events")
 
 local function update_idle()
-    if mp.get_property_native("idle-active") then
-        
-        -- ENGINE LEVEL KILL SWITCH: Physically disables the default UI component
-        mp.set_property("osc", "no")
-        
-        -- Draw your custom text
-        local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HB9802A&}MPV PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
-        overlay.data = ass
-        overlay:update()
+if mp.get_property_native("idle-active") then
+    -- Draw your custom blue text
+    local ass = "{\\an5}{\\fs45}{\\b1}{\\c&HFF8800&}MPV AUDIO PLAYER\\N\\N{\\b0}{\\fs30}{\\c&HFFFFFF&}Drop a track here to play"
+    overlay.data = ass
+    overlay:update()
     else
-        -- Physically turn the default UI back on when music starts
-        mp.set_property("osc", "yes")
+        -- Remove the text when media starts (Notice we removed the osc=yes command here!)
         overlay:remove()
-    end
-end
+        end
+        end
 
 mp.observe_property("idle-active", "bool", update_idle)
